4b9a0f82aa49e38675946df2d463c68d691a110e
dngrz <clodoaldosanchez@gmail.com>
2016-07-04 00:03:13 -0500

    Visualizando la Lista de Topics en el Header

--- imgur-client/src/components/header.jsx
+++ imgur-client/src/components/header.jsx
@@ -1,8 +1,22 @@
 var React = require('react');
 var Router = require('react-router');
+var Reflux = require('reflux');
 var Link = Router.Link;
+var TopicStore = require('../stores/topic-store');
+var Actions = require('../actions');
 
 module.exports = React.createClass({
+  mixins: [
+    Reflux.listenTo(TopicStore, 'onChange')
+  ],
+  getInitialState: function(){
+    return {
+      topics: []
+    }
+  },
+  componentWillMount: function(){
+    Actions.getTopics();
+  },
     render: function() {
         return <nav className="navbar navbar-default header">
             <div className="container-fluid">
@@ -10,14 +24,26 @@ module.exports = React.createClass({
                     Imgur Browser
                 </Link>
                 <ul className="nav navbar-nav navbar-right">
-                    <li>
-                        <a>Topic #1</a>
-                    </li>
+                    {this.renderTopics()}
                 </ul>
 
             </div>
         </nav>
 
+    },
+    renderTopics: function(){
+      return this.state.topics.slice(0, 4).map(function(topic){
+        return <li key={topic.id}>
+          <Link to={"topics/"+topic.id}>
+            {topic.name}
+          </Link>
+        </li>
+      });
+    },
+    onChange: function(event, topics){
+      this.setState({
+        topics: topics
+      });
     }
 
 });
