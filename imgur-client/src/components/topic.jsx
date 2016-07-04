083208521cce759758d10878f3e7c3f1310a5f94
dngrz <clodoaldosanchez@gmail.com>
2016-07-04 00:32:51 -0500

    Creando image-store

--- imgur-client/src/components/topic.jsx
+++ imgur-client/src/components/topic.jsx
@@ -1,7 +1,28 @@
 var React = require('react');
+var Actions = require('../actions');
+var ImageStore = require('../stores/image-store');
+var Reflux = require('reflux');
 
 module.exports = React.createClass({
+  mixins: [
+    Reflux.listenTo(ImageStore, 'onChange')
+  ],
+  getInitialState: function(){
+    return {
+      images: []
+    }
+  },
+  componentWillMount: function(){
+    Actions.getImages(this.props.params.id);
+  },
   render: function(){
-    return <div>Topic</div>
+    return <div>
+      Topic con ID {this.props.params.id}      
+    </div>
+  },
+  onChange: function(event, images){
+    this.setState({
+      images: images
+    })
   }
 });
